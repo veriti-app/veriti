fbd538165030f13d7a368a394056709b22b95884
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-09 15:05:36 -0500

    UI components

---
+++ client/src/components/Card.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Card = () => {
+    return ( 
+        <div className="">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="/">
+                    <img className="rounded-t-lg" src="https://www.cdc.gov/stem/image/CMDDC.png?_=40466" alt="" />
+                </a>
+                <div className="p-5">
+                    <span className="mb-1 bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+                        Healthcare
+                        </span>
+                    <a href="/">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Noteworthy technology acquisitions 2021
+                            </h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        </p>
+                    <a href="/" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        Save
+                        </a>
+                    <a href="/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Donate
+                        </a>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Card;
