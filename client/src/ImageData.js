70d6c415288aa08e1d2f97ed5eb7837645e88631
Olivia Conley <oliviarconley@gmail.com>
2022-12-08 21:52:24 -0500

    Set up files for components and pages

---
+++ client/src/ImageData.js
@@ -0,0 +1,11 @@
+export default imgData = [
+    {
+        orgName: "Save the Children", 
+        imgLink: "https://www.savethechildren.org/content/dam/global/images/countries/somalia/somalia-hunger-ahmed-ch1535822-sq.jpg/_jcr_content/renditions/cq5dam.webp_thumbnail.2562.2562.webp"
+    }, 
+    {
+        orgName: "", 
+        imgLink: ""
+    }
+]
+
