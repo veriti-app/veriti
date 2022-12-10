f54e02939c02ef7b0c41eb8290a871b3506743b5
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-10 17:24:59 -0500

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
       <div className="container mx-auto px-4">
         <div className="py-4">
           <h1 className="text-3xl">
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,21 +2,29 @@ import React from "react";
 
 const Header = () => {
     return ( 
-        <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-      style="background-color: rgba(0, 0, 0, 0.75)">
-      <div class="flex justify-center items-center h-full">
-        <div class="text-center text-white px-6 md:px-12">
-          <h1 class="text-5xl font-bold mt-0 mb-6">Heading</h1>
-          <h3 class="text-3xl font-bold mb-8">Subeading</h3>
-          <button type="button"
-            class="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-            data-mdb-ripple="true" data-mdb-ripple-color="light">
-            Get started
-          </button>
+        <div className="">
+            <div className="relative overflow-hidden bg-no-repeat bg-cover" style={{
+                height: '350px',
+            }}>
+            <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+            style={{
+                height: '350px',
+                color: '#000000',
+            }}>
+            <div className="flex justify-center items-center h-full">
+                <div className="text-center text-white px-6 md:px-12">
+                <h1 className="text-5xl font-bold mt-0 mb-6">Heading</h1>
+                <h3 className="text-3xl font-bold mb-8">Subeading</h3>
+                <button type="button"
+                    className="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                    data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    Get started
+                </button>
+                </div>
+            </div>
+            </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
      );
 }
  
