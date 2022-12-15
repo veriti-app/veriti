921e8f11d5e7ae563127871abd5916aad2be5517
isayani <isayani95@gmail.com>
2022-12-14 23:13:40 -0500

    init mutations in utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,3 +22,52 @@ export const ADD_USER = gql`
     }
   }
 `;
+export const ADD_DONATION = gql`
+mutation Mutation($donationAmount: Float!, $donationDate: String!, $charity: ID!) {
+    addDonation(donationAmount: $donationAmount, donationDate: $donationDate, charity: $charity) {
+      _id
+      charity {
+        _id
+        ein
+        link
+        location
+        mission
+        name
+      }
+      donationAmount
+      donationDate
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const SAVE_CHARITY = gql`
+mutation Mutation($charity: ID!) {
+    saveCharity(charity: $charity) {
+      _id
+      username
+      categories {
+        _id
+      }
+      charities {
+        _id
+      }
+    }
+  }
+`;
+export const UNSAVE_CHARITY = gql`
+mutation Mutation($user: ID!, $charity: ID!) {
+    unsaveCharity(user: $user, charity: $charity) {
+      _id
+      username
+      categories {
+        _id
+      }
+      charities {
+        _id
+      }
+    }
+  }
+`;
