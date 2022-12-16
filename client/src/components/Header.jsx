ca9adb1318d041b562bdb5837d1380c47a271e0d
isayani <isayani95@gmail.com>
2022-12-16 14:42:47 -0500

    init card structure/styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CardNoCTA from "./CardNoCTA"
 
 function Header() {
     return (
@@ -17,19 +18,26 @@ function Header() {
                                 <h1 style={{ textTransform: "uppercase" }} className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-white font-bold leading-tight">
                                     Value your verity
                                     </h1>
-                                <h5 className="text-lg text-center text-white font-bold leading-tight py-2"> Build your own charity portfolio </h5>
+                                <h5 className="text-lg text-center text-white font-bold leading-tight py-2"> Build your own charitable portfolio </h5>
                             </div>
                             <div className="flex justify-center items-center mb-10 sm:mb-20">
                             <Link to="/login" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-indigo-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                            Sign Up
+                            Start Donating Today
                             </Link>
-                            <Link to="/explore" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-indigo-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            {/* <Link to="/explore" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-indigo-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                             Explore
-                            </Link>
+                            </Link> */}
                             </div>
                         </div>
                 </div>
             </dh-component>
+            <div className="justify-center flex px-4 py-6 mt-10 space-x-8">
+            <CardNoCTA/>
+            <CardNoCTA/>
+            <CardNoCTA/>
+            <CardNoCTA/>
+            <CardNoCTA/>
+            </div>
         </div>
     )
 }
