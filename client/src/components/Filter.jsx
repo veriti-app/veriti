392d5495a590b86b9c74dda835ddfe7711fa2a04
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-15 20:41:48 -0500

    style

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Filter = () => {
     return ( 
-        <div className="container py-4">
+        <div className="p-4">
             <button id="dropdownBgHoverButton" data-dropdown-toggle="dropdownBgHover" className="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800" type="button">Category<svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
 
             <div id="dropdownBgHover" className="hidden z-10 w-48 bg-white rounded shadow dark:bg-gray-700">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,9 @@ export default function Home() {
   return (
     <div className="home">
       <Header />
-      <Filter />
+      <div className="float-right">
+        <Filter />
+      </div>
       <Card />
       <Modal />
     </div>
