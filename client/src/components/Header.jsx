fafd7e5f3aef00077986966042e1b75471e5abfe
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-15 22:17:35 -0500

    link to explore page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
                     height: "100vh"
                     }}>
             <dh-component>
-                <div className="w-full px-4">
+                <div className="w-full px-4 justify-center">
                     <div className="mt-2 relative rounded-lg container mx-auto flex flex-col items-center pt-12 sm:pt-24 pb-24 sm:pb-32 md:pb-48 lg:pb-56 xl:pb-64">
                             {/* <img className="rounded-lg " src="https://www.cdc.gov/training/images/1330x370_etsb-training_10-15.png?_=24719" alt="bg" /> */}
                             <div className="mt-8 w-11/12 sm:w-2/3 mb-5 sm:mb-10">
@@ -23,7 +23,7 @@ function Header() {
                             <Link to="/login" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-indigo-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                             Sign Up
                             </Link>
-                            <Link to="/" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-indigo-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            <Link to="/explore" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-indigo-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                             Explore
                             </Link>
                             </div>
