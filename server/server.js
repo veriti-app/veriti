7410f0bcd5f68830f7da15c3e52d98a451152133
Indigo <indigo.crandell@gmail.com>
2022-12-15 21:15:07 -0500

    saving work for stripe set up

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
@@ -21,6 +22,31 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+const YOUR_DOMAIN = "http://localhost:3000";
+
+app.post("/create-checkout-session", async (req, res) => {
+  const price = await stripe.prices.create({
+    product: "donation",
+    unit_amount: req.body.amount,
+    currency: "usd",
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: price,
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -38,3 +64,43 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
+
+// // This is a public sample test API key.
+// // Don’t submit any personally identifiable information in requests made with this key.
+// // Sign in to see your own test API key embedded in code samples.
+
+// const express = require("express");
+// const app = express();
+// // This is a public sample test API key.
+// // Don’t submit any personally identifiable information in requests made with this key.
+// // Sign in to see your own test API key embedded in code samples.
+// const stripe = require("stripe")('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+// app.use(express.static("public"));
+// app.use(express.json());
+
+// const calculateOrderAmount = (items) => {
+//   // Replace this constant with a calculation of the order's amount
+//   // Calculate the order total on the server to prevent
+//   // people from directly manipulating the amount on the client
+//   return 1400;
+// };
+
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { items } = req.body;
+
+//   // Create a PaymentIntent with the order amount and currency
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: calculateOrderAmount(items),
+//     currency: "usd",
+//     automatic_payment_methods: {
+//       enabled: true,
+//     },
+//   });
+
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
+
+// app.listen(4242, () => console.log("Node server listening on port 4242!"));
