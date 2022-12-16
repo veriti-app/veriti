68a89780220c1cbfd51a2e76a39cb226e21d55fc
Indigo <indigo.crandell@gmail.com>
2022-12-15 21:35:36 -0500

    Attempting new format for stripe so that we get the entire payment view as soon as we navigate to checkout

--- client/src/components/Stripe.jsx
+++ client/src/components/Stripe.jsx
@@ -1,60 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { json } from "react-router-dom";
 import CardNoCTA from "./CardNoCTA";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Stripe = () => {
-  const [amount, setAmount] = useState();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetch("/create-checkout-session", {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
       method: "POST",
-      body: JSON.stringify({ amount: amount }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
   };
 
   return (
-    <section>
-      <form>
-        <input
-          type="number"
-          onChange={(event) => setAmount(event.target.value)}
-        ></input>
-        <button
-          onClick={handleSubmit}
-          type="submit"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
-        >
-          Checkout
-        </button>
-
-        <div className="py-6">
-          <CardNoCTA />
-          <div>
-            <section>
-              <div className="product">
-                <img
-                  src="https://i.imgur.com/EHyR2nP.png"
-                  alt="The cover of Stubborn Attachments"
-                />
-                <div className="description">
-                  <h3>Stubborn Attachments</h3>
-                  <h5>$20.00</h5>
-                </div>
-              </div>
-              <form action="/create-checkout-session" method="POST">
-                <button type="submit">Checkout</button>
-              </form>
-            </section>
-            {/* <section>
-              <p>{message}</p>
-            </section> */}
-          </div>
-        </div>
-      </form>
-    </section>
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
   );
+
+  //   const [amount, setAmount] = useState();
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     fetch("/create-payment-intent", {
+  //       method: "POST",
+  //       body: JSON.stringify({ amount: amount }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => console.log(data));
+  //   };
+
+  //   return (
+  //     <section>
+  //       <form>
+  //         <input
+  //           type="number"
+  //           onChange={(event) => setAmount(event.target.value)}
+  //         ></input>
+  //         <button
+  //           onClick={handleSubmit}
+  //           type="submit"
+  //           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
+  //         >
+  //           Checkout
+  //         </button>
+
+  //         <div className="py-6">
+  //           <CardNoCTA />
+  //           <div>
+  //             <section>
+  //               <div className="product">
+  //                 <img
+  //                   src="https://i.imgur.com/EHyR2nP.png"
+  //                   alt="The cover of Stubborn Attachments"
+  //                 />
+  //                 <div className="description">
+  //                   <h3>Stubborn Attachments</h3>
+  //                   <h5>$20.00</h5>
+  //                 </div>
+  //               </div>
+  //               <form action="/create-checkout-session" method="POST">
+  //                 <button type="submit">Checkout</button>
+  //               </form>
+  //             </section>
+  //             {/* <section>
+  //               <p>{message}</p>
+  //             </section> */}
+  //           </div>
+  //         </div>
+  //       </form>
+  //     </section>
+  //   );
 };
 
 export default Stripe;
--- server/server.js
+++ server/server.js
@@ -22,30 +22,45 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-const YOUR_DOMAIN = "http://localhost:3000";
-
-app.post("/create-checkout-session", async (req, res) => {
-  const price = await stripe.prices.create({
-    product: "donation",
-    unit_amount: req.body.amount,
+app.post("/create-payment-intent", async (req, res) => {
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: req.body,
     currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
   });
 
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: price,
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  res.send({
+    clientSecret: paymentIntent.client_secret,
   });
-
-  res.redirect(303, session.url);
 });
+
+// const YOUR_DOMAIN = "http://localhost:3000";
+
+// app.post("/create-checkout-session", async (req, res) => {
+//   const price = await stripe.prices.create({
+//     product: "donation",
+//     unit_amount: req.body.amount,
+//     currency: "usd",
+//   });
+
+//   const session = await stripe.checkout.sessions.create({
+//     line_items: [
+//       {
+//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+//         price: price,
+//         quantity: 1,
+//       },
+//     ],
+//     mode: "payment",
+//     success_url: `${YOUR_DOMAIN}?success=true`,
+//     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+//   });
+
+//   res.redirect(303, session.url);
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
@@ -65,42 +80,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
 
-// // This is a public sample test API key.
-// // Don’t submit any personally identifiable information in requests made with this key.
-// // Sign in to see your own test API key embedded in code samples.
-
-// const express = require("express");
-// const app = express();
-// // This is a public sample test API key.
-// // Don’t submit any personally identifiable information in requests made with this key.
-// // Sign in to see your own test API key embedded in code samples.
-// const stripe = require("stripe")('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-
-// app.use(express.static("public"));
-// app.use(express.json());
-
-// const calculateOrderAmount = (items) => {
-//   // Replace this constant with a calculation of the order's amount
-//   // Calculate the order total on the server to prevent
-//   // people from directly manipulating the amount on the client
-//   return 1400;
-// };
-
-// app.post("/create-payment-intent", async (req, res) => {
-//   const { items } = req.body;
-
-//   // Create a PaymentIntent with the order amount and currency
-//   const paymentIntent = await stripe.paymentIntents.create({
-//     amount: calculateOrderAmount(items),
-//     currency: "usd",
-//     automatic_payment_methods: {
-//       enabled: true,
-//     },
-//   });
-
-//   res.send({
-//     clientSecret: paymentIntent.client_secret,
-//   });
-// });
-
-// app.listen(4242, () => console.log("Node server listening on port 4242!"));
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
