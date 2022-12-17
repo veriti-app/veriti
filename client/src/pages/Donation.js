c45ac918cc5c5bc6f21a92338c15f467ee353e41
Insha <107900180+isayani@users.noreply.github.com>
2022-12-17 18:07:31 -0500

    added stripe useState and Effect on donation page

--- client/src/pages/Donation.js
+++ client/src/pages/Donation.js
@@ -1,34 +1,61 @@
-import React from "react";
-import Stripe from "../components/Stripe";
+import React, { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "../components/CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
 export default function Donation() {
-    return (
-        <div className="flex flex-col">
-        <h1 className="text-3xl m-5">How much do you want to donate?</h1>
-        <div>
-          <form className="flex flex-col gap-4 ml-6 mb-2 border-gray-200 rounded-lg">
-              <div>
-                <div className="m-2 block">
-                  <label
-                    htmlFor="donation"
-                    value="Donation amount"
-                  />
-                </div>
-                <input className="rounded-md border-gray-200"
-                  id="donation"
-                  type="number"
-                  name="price" 
-                  placeholder="$00.00"
-                  required={true}
-                />
-              </div>
-    
-                <div className="m-2">
-                    < Stripe />
-                </div>
-            </form>
-        </div>  
-      </div>
-    );
-}
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
 
+  useEffect(() => {
+    fetch("/config").then(async (r) => {
+      const { publishableKey } = await r.json();
+
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("/create-payment-intent", {
+      method: "POST",
+      body: JSON.stringify({}),
+    }).then(async (r) => {
+      const { clientSecret } = await r.json();
+      
+      setClientSecret(clientSecret);
+      console.log("RESPONSE", r)
+      return r
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col">
+      <h1 className="text-3xl m-5">How much would you like to give?</h1>
+      <div>
+        <form className="flex flex-col gap-4 ml-6 mb-2 border-gray-200 rounded-lg">
+          <div>
+            <div className="m-2 block">
+              <label htmlFor="donation" value="Donation amount" />
+            </div>
+            <input
+              className="rounded-md border-gray-200"
+              id="donation"
+              type="number"
+              name="price"
+              placeholder="$25.00"
+              required={true}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+    
+      {stripePromise && clientSecret && (
+        <Elements stripe={stripePromise} options={{ clientSecret }}>
+        <CheckoutForm />
+        </Elements>
+      )}
+    </>
+  );
+}
