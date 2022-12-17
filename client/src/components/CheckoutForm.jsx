90e03c2bd90d514d696f7b9fd50e040392a00897
Insha <107900180+isayani@users.noreply.github.com>
2022-12-17 16:45:06 -0500

    checkout form tested and working

---
+++ client/src/components/CheckoutForm.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+
+export default function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [message, setMessage] = useState(null);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsProcessing(true);
+    // TO-DO: Window Location (Success message)
+    const { error, paymentIntent } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `${window.location.origin}/portfolio`,
+      },
+      redirect: "if_required",
+    });
+
+    if (error) {
+      setMessage(error.message);
+    } else if (paymentIntent && paymentIntent.status === "succeeded") {
+      setMessage("Congrats! Your donation " + paymentIntent.status + "🎉");
+    } else {
+      setMessage("An unexpected error occured.");
+    }
+
+    setIsProcessing(false);
+  };
+
+  return (
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <PaymentElement id="payment-element" />
+      <button disabled={isProcessing || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isProcessing ? "Processing ... " : "Pay now"}
+        </span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
+    </form>
+  );
+}
