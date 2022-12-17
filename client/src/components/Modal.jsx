0d8a539b4242ef544d1a4d060a9f09d5f22dbf5d
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-17 13:31:24 -0500

    Update Modal.jsx
    change p

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -41,11 +41,7 @@ export default function Modal() {
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                    content
                   </p>
                 </div>
                 {/*footer*/}
