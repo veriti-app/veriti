59969f5c2b39f7a6a899d3f23fc1bcee18cf4119
Olivia Conley <oliviarconley@gmail.com>
2022-12-17 17:41:44 -0500

    Tax form download button

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TaxDownload from "./TaxDownload"; 
 
 const Summary = () => {
   return (
@@ -41,7 +42,7 @@ const Summary = () => {
           <div className="max-w-sm">
                   <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
                   <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-                  <span>Download Tax Forms</span>
+                  <span><TaxDownload /></span>
                   </button>
           </div>
           <div className="max-w-sm">
