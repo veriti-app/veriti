916446bbce15e6cc8e9bd7e8982b4cde146c9d2e
Insha <107900180+isayani@users.noreply.github.com>
2022-12-17 19:46:00 -0500

    completed Donation with CheckoutForm component

--- client/src/pages/Donation.js
+++ client/src/pages/Donation.js
@@ -21,39 +21,38 @@ export default function Donation() {
       body: JSON.stringify({}),
     }).then(async (r) => {
       const { clientSecret } = await r.json();
-      
+
       setClientSecret(clientSecret);
-      console.log("RESPONSE", r)
-      return r
+      console.log("RESPONSE", r);
+      return r;
     });
   }, []);
 
   return (
     <>
       <div className="flex flex-col">
-      <h1 className="text-3xl m-5">How much would you like to give?</h1>
-      <div>
-        <form className="flex flex-col gap-4 ml-6 mb-2 border-gray-200 rounded-lg">
-          <div>
-            <div className="m-2 block">
-              <label htmlFor="donation" value="Donation amount" />
+        <h1 className="text-3xl m-5">How much would you like to give?</h1>
+        <div>
+          <form className="flex flex-col gap-4 ml-6 mb-2 border-gray-200 rounded-lg">
+            <div>
+              <div className="m-2 block">
+                <label htmlFor="donation" value="Donation amount" />
+              </div>
+              <input
+                className="rounded-md border-gray-200"
+                id="donation"
+                type="number"
+                name="price"
+                placeholder="$25.00"
+                required={true}
+              />
             </div>
-            <input
-              className="rounded-md border-gray-200"
-              id="donation"
-              type="number"
-              name="price"
-              placeholder="$25.00"
-              required={true}
-            />
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-    
       {stripePromise && clientSecret && (
         <Elements stripe={stripePromise} options={{ clientSecret }}>
-        <CheckoutForm />
+          <CheckoutForm />
         </Elements>
       )}
     </>
