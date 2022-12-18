0d49fbf3a48d8f1dfad1c52444cc7661e90fb661
Olivia Conley <oliviarconley@gmail.com>
2022-12-18 01:12:24 -0500

    Download .xlsx feature functionality

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import DownloadXlsx from "./DownloadXlsx";
 
 const Summary = () => {
   return (
     // Dashboard container
     <div className="bg-indigo-100 rounded-lg px-4">
-      <h1 className="my-2 py-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Summary</h1>
-    {/* Summary */}
+      <h1 className="my-2 py-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Summary
+      </h1>
+      {/* Summary */}
       <div className="flex flex-wrap flex-row space-x-2">
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <div className="p-5">
@@ -35,21 +38,27 @@ const Summary = () => {
           </div>
         </div>
       </div>
-    
-    {/* Downloads */}
+
+      {/* Downloads */}
       <div className="flex flex-wrap p-4 space-x-2">
-          <div className="max-w-sm">
-                  <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                  <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-                  <span>Download Tax Forms</span>
-                  </button>
-          </div>
-          <div className="max-w-sm">
-                  <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                  <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-                  <span>Download Donation Summary</span>
-                  </button>
-          </div>
+        <div className="max-w-sm">
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg
+              className="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>Download Tax Forms</span>
+          </button>
+        </div>
+        <div className="max-w-sm">
+            <span>
+              <DownloadXlsx />
+            </span>
+         
+        </div>
       </div>
     </div>
   );
