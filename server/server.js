bc62e7873d609a71a969421a0a0a8d0fc9b05a7a
VaishaliQA <patelvaishali1890@gmail.com>
2022-12-19 10:58:43 -0800

    Updated for stripe

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const env = require("dotenv").config({ path: "./.env" });
+require("dotenv").config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
