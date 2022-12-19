6ec5924ae6c1c23db2cef3dcb9a1988a54e6a1a0
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-18 23:43:58 -0500

    styling close button

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { SINGLE_CHARITY } from "../utils/queries";
 
-export default function Modal() {
+export default function Modal(props) {
   const [showModal, setShowModal] = React.useState(false);
-  const { data } = useQuery(SINGLE_CHARITY);
+  const id = props.charityId;
+  console.log("charity id", id);
+  const { data } = useQuery(SINGLE_CHARITY, {
+    variables: { charityId: id },
+  });
+  console.log("data", data)
   const charity = data?.charity || [];
 
   return (
@@ -21,7 +26,7 @@ export default function Modal() {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-sm">
+            <div className="relative w-auto my- mx-auto max-w-sm">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
@@ -37,7 +42,7 @@ export default function Modal() {
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex items-center justify-end p-6 rounded-b">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
