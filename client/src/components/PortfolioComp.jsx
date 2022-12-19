774bf29a983d6bd0c62de5d1fcae7a63550b760b
Indigo <indigo.crandell@gmail.com>
2022-12-19 16:45:44 -0500

    0

--- client/src/components/PortfolioComp.jsx
+++ client/src/components/PortfolioComp.jsx
@@ -1,30 +1,76 @@
 import React from "react";
+import TaxDownload from "./TaxDownload";
+import DownloadXlsx from "./DownloadXlsx";
 import { useQuery } from "@apollo/client";
-import EmptyState from "./EmptyState";
-import CardSaved from "../components/CardSaved";
-import { QUERY_ME } from "../utils/queries";
+import { USER_SUMMARY } from "../utils/queries";
 
-const Portfolio = () => {
-  // checking user has saved user charities or not
-  const { data } = useQuery(QUERY_ME);
-  // Changed this from data == 0  (data was undefined)
-  if (!data) {
-    return (
-      <div className="">
-        <EmptyState />
+const Summary = () => {
+  // Extract data from USER_SUMMARY
+  const { data } = useQuery(USER_SUMMARY);
+  const donations = data?.me.donations || [];
+  const categories = data?.me.categories || [];
+
+  // Total categories calculation
+  const categoryLength = categories.length;
+
+  // Total donation spent
+  const donationTotal = donations.reduce(
+    (total, obj) => obj.donationAmount + total,
+    0
+  );
+
+  return (
+    // Dashboard container
+    <div className="bg-indigo-100 rounded-lg px-4">
+      <h1 className="my-2 py-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Summary
+      </h1>
+      {/* Summary */}
+      <div className="flex flex-wrap flex-col items-center md:items-start md:flex-row lg:flex-row space-x-2 gap-2">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-5">
+            <a href="/">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Your Contribution
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              You've donated a total of{" "}
+              <span className="mb-1 bg-indigo-100 text-indigo-800 text-s font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-800">
+                ${donationTotal}
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-5">
+            <a href="/">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Your Portfolio
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              You've donated across {categoryLength} categories.
+            </p>
+          </div>
+        </div>
       </div>
-    );
-  } else {
-    // if user has saved charities then it's dispalyed on card saved component
-    return (
-      <div className="rounded-lg px-4">
-        <h1 className="py-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Your Saved Charities
-        </h1>
-        <CardSaved />
+
+      {/* Downloads */}
+      <div className="flex flex-col md:flex-row items-center md:items-center py-2 gap-2">
+        <div className="max-w-sm">
+          <span>
+            <TaxDownload />
+          </span>
+        </div>
+        <div className="max-w-sm">
+          <span>
+            <DownloadXlsx />
+          </span>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
-export default Portfolio;
+export default Summary;
