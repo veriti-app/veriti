5548a40c271618ef76c3b3b271663de4dce5cc35
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-18 22:24:13 -0500

    delete x button

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -29,14 +29,6 @@ export default function Modal() {
                   <h3 className="text-3xl font-semibold">
                     {charity}
                   </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
