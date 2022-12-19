f08f50cdbf0cb510721e18faa386fa9acbcc7408
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-12-18 23:45:15 -0500

    modal styling update

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -26,7 +26,7 @@ export default function Modal(props) {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my- mx-auto max-w-sm">
+            <div className="relative w-auto my- mx-auto max-w-md">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
